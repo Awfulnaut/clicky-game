7043dc1916a870ec545f497c9870bbdc8e401947
Cadin McQueen <cadin.mcqueen@gmail.com>
2018-12-19 20:50:14 -0500

    Updates

---
+++ precache-manifest.912a64b9c52b996eb3e64a17aef45ca9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2b535f6e6bfc0f0a0b696ce589c33f1",
+    "url": "/clicky-game/static/media/mildred-11.d2b535f6.jpg"
+  },
+  {
+    "revision": "53fbf1ece16ca7502893",
+    "url": "/clicky-game/static/css/main.7127464b.chunk.css"
+  },
+  {
+    "revision": "eda90923e05acbf3134a",
+    "url": "/clicky-game/static/js/1.eda90923.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "bf5f1742dd55fece3968e82ca7fe765d",
+    "url": "/clicky-game/static/media/mildred-1.bf5f1742.jpg"
+  },
+  {
+    "revision": "f3000389c13eeecedb8ed5a713099280",
+    "url": "/clicky-game/static/media/mildred-10.f3000389.jpg"
+  },
+  {
+    "revision": "53fbf1ece16ca7502893",
+    "url": "/clicky-game/static/js/main.53fbf1ec.chunk.js"
+  },
+  {
+    "revision": "8b01ca8210969c092785beb86cb1174a",
+    "url": "/clicky-game/static/media/mildred-12.8b01ca82.jpg"
+  },
+  {
+    "revision": "cf688f22bfa875efc9b4cf097a665a09",
+    "url": "/clicky-game/static/media/mildred-2.cf688f22.jpg"
+  },
+  {
+    "revision": "d3b2fd98f42dd8143a0a9e1f7b42cd0b",
+    "url": "/clicky-game/static/media/mildred-3.d3b2fd98.jpg"
+  },
+  {
+    "revision": "e008f4116d09e46fa45615440e2a66de",
+    "url": "/clicky-game/static/media/mildred-7.e008f411.jpg"
+  },
+  {
+    "revision": "83d7238c739c9f633fc09faf42aa2d6e",
+    "url": "/clicky-game/index.html"
+  }
+];
